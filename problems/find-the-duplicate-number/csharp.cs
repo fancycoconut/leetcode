bfd8595e8bc73b4102da3b13ce554ba36fcc91ca
Kawai Wong <kawai.wong@pushpay.com>
2024-12-06 15:44:02 +1300

    Add exercise and solution for find the duplicate number

---
+++ problems/find-the-duplicate-number/csharp.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int FindDuplicate(int[] nums) {
+        var set = new HashSet<int>();
+        foreach (var num in nums)
+        {
+            if (set.Contains(num)) return num;
+            set.Add(num);
+        }
+
+        return -1;
+    }
+}
