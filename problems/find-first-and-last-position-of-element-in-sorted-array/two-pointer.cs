3bc45774a1b43299275f2abb9b22a56a8b55ddff
fancycoconut <zodiacdagreat@outlook.com>
2025-01-17 00:00:04 +1300

    Add first and last position of element in sorted array

---
+++ problems/find-first-and-last-position-of-element-in-sorted-array/two-pointer.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public int[] SearchRange(int[] nums, int target) {
+        if (nums.Length == 0) return new int[] { -1, -1 };
+
+        var left = 0;
+        var right = nums.Length - 1;
+        while (left < nums.Length && right >= 0)
+        {
+            var num1 = nums[left];
+            var num2 = nums[right];
+            if (num1 == target && num2 == target) return new int[] { left, right };
+
+            if (num1 != target)
+            {
+                left++;
+                continue;
+            }
+
+            if (num2 != target)
+            {
+                right--;
+                continue;
+            }
+        }
+
+
+        return new int[] { -1, -1 };
+    }
+}
