37e9108a032566b93bf148003ff961e998477a5e
fancycoconut <zodiacdagreat@outlook.com>
2025-01-18 23:35:37 +1300

    Add documentation

--- problems/climbing-stairs/dynamic-programming.py
+++ problems/climbing-stairs/dynamic-programming.py
@@ -1,3 +1,14 @@
+# 1. Define the objective function
+# f(n) => the number of distinct ways to reach the n-th stair
+# 2. Identity base cases
+# f(0) = 1
+# f(1) = 1
+# 3. Recurrence relation:
+# f(n) = f(n - 1) + f(n - 2) 
+# 4. Order of computation
+# bottom-up
+# 5. Location of the answer
+# f(n)
 class Solution:
     cache = {}
 
