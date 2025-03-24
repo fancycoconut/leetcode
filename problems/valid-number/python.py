88f91bf3edb1a015a0859384afd392aa76c8d9ee
fancycoconut <zodiacdagreat@outlook.com>
2025-03-25 00:47:11 +1300

    Add valid number work in progress

---
+++ problems/valid-number/python.py
@@ -0,0 +1,109 @@
+class Solution:
+    def isNumber(self, s: str) -> bool:
+        num = s.upper()
+
+        if self.validateSymbols(num) == False:
+            return False
+
+        nums = "0123456789"
+        hasNumbers = False
+        for n in num:
+            if n in nums:
+                hasNumbers = True
+
+        if hasNumbers == False:
+            return False
+
+        return self.isInteger(num) or self.isDecimal(num)
+
+    def validateSymbols(self, num: str) -> bool:
+        validSymbols = "+-0123456789.E"
+        for n in num:
+            if n not in validSymbols:
+                return False
+        return True
+
+    def isInteger(self, num: str) -> bool:
+        if "." in num:
+            return False
+
+        if self.hasValidSigns(num) == False:
+            return False
+        if self.hasValidExponent(num) == False:
+            return False
+        
+        return True
+
+    def isDecimal(self, num: str) -> bool:
+        if num.count(".") != 1:
+            return False
+        if num == ".":
+            return False
+
+        if self.hasValidSigns(num) == False:
+            return False
+        if self.hasValidExponent(num) == False:
+            return False
+
+        return True
+
+    def hasValidSigns(self, num: str) -> bool:
+        if "+" not in num and "-" not in num:
+            return True
+
+        if "E" not in num:
+            if num.count("+") > 1 or num.count("-") > 1:
+                return False
+            if "+" in num and "-" in num:
+                return False
+            if num.find(".") != -1 and num.find("-") > num.find("."):
+                return False            
+
+        prev = num[0]
+        nums = "0123456789"
+        for n in num:
+            if prev in nums and n == "-":
+                return False
+            if prev in nums and n == "+":
+                return False
+            if prev == "-" and n == "+":
+                return False
+            if prev == "+" and n == "-":
+                return False
+            if prev == "." and n == "-":
+                return False
+            if prev == "." and n == "+":
+                return False
+            if prev == "-" and n == "E":
+                return False
+            if prev == "+" and n == "E":
+                return False
+            prev = n
+
+        return True
+
+    def hasValidExponent(self, num: str) -> bool:
+        if "E" not in num:
+            return True
+
+        if num[-1] == "E":
+            return False
+        if num[-1] == "-":
+            return False
+        if num[-1] == "+":
+            return False
+        if num.find(".") > num.find("E"):
+            return False
+
+        hasPlus = num.find("+") > num.find("E")
+        hasMinus = num.find("-") > num.find("E")
+        prev = num[0]
+        nums = "+-0123456789"
+        for n in num:
+            if n == "E" and prev not in nums:
+                return False
+            prev = n
+        
+        if hasMinus and hasPlus:
+            return False
+        return True
