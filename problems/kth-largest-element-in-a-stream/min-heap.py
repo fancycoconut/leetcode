e6cde73adebc81fbade1e9b1dabeb1177ebbc721
Kawai Wong <kawai.wong@pushpay.com>
2025-04-24 14:50:48 +1200

    Add kth largest element in a stream

---
+++ problems/kth-largest-element-in-a-stream/min-heap.py
@@ -0,0 +1,24 @@
+class KthLargest:
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.min_heap = []
+
+        for num in nums:
+            self.add(num)
+
+    def add(self, val: int) -> int:
+        # Add to our min_heap if we haven't processed k elements yet
+        # or if val is greater than the top element (the k-th largest)
+        if len(self.min_heap) < self.k or self.min_heap[0] < val:
+            heapq.heappush(self.min_heap, val)
+
+        if len(self.min_heap) > self.k:
+            heapq.heappop(self.min_heap)
+
+        #print(self.min_heap)
+        return self.min_heap[0]
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
