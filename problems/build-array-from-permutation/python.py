f0d7a4e07798eb4620cd90d6b65787965272e9a9
Kawai Wong <kawai.wong@pushpay.com>
2025-05-06 21:51:47 +1200

    Add build array from permutation

---
+++ problems/build-array-from-permutation/python.py
@@ -0,0 +1,7 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        output = []
+        for num in nums:
+            output.append(nums[num])
+
+        return output
