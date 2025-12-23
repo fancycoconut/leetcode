0b4c6dc2f3ab1bcc69087ee68fae7fcae265bb19
fancycoconut <zodiacdagreat@outlook.com>
2025-12-24 09:58:00 +1300

    Add recursive solution for binary level order traversal

---
+++ problems/binary-tree-level-order-traversal/recursive-breadth-first-search.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        levels = []
+        if not root:
+            return levels
+
+        def traverse(node: Optional[TreeNode], level: int) -> None:
+            if not node:
+                return
+
+            if level == len(levels):
+                levels.append([])
+            
+            levels[level].append(node.val)
+
+            traverse(node.left, level + 1)
+            traverse(node.right, level + 1)
+        
+        traverse(root, 0)
+        return levels
