c84b15adecfa03067612cae134a5eb2075945699
fancycoconut <zodiacdagreat@outlook.com>
2026-01-09 22:42:58 +1300

    Add course schedule ii

---
+++ problems/course-schedule-ii/dfs.py
@@ -0,0 +1,35 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        courseMap = { course:[] for course in range(numCourses) }
+        for course,prereq in prerequisites:
+            courseMap[course].append(prereq)
+        print(courseMap)
+
+        output = []
+        uniqueOutput = set()
+        visited = set()
+        def dfs(course) -> bool:
+            if course in visited:
+                return False
+            deps = courseMap[course]
+            if len(deps) == []:
+                return True
+            
+            visited.add(course)
+            for dep in deps:
+                if not dfs(dep):
+                    return False
+            
+            visited.remove(course)
+            courseMap[course] = []
+
+            if course not in output:
+                output.append(course)
+                uniqueOutput.add(course)
+            return True
+
+        for course in range(numCourses):
+            if not dfs(course):
+                return []
+
+        return output
